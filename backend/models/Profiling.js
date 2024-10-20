b3d40bcd193ef138c52eaab7f40f6ccf653e4398
sahilbd1907 <142887739+sahilbd1907@users.noreply.github.com>
2024-10-20 14:33:48 +0530

    creating Profiling.js

---
+++ backend/models/Profiling.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const profilingSchema = new mongoose.Schema({
+    profileImage: String,
+    name: String,
+    experience: String,
+    profession: String,
+});
+const Profiling = mongoose.model('Profiling', profilingSchema);
+
+module.exports = Profiling;
