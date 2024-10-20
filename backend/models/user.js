49b94c4ee448313b9eecb4998e5612cf1da348a9
sahilbd1907 <142887739+sahilbd1907@users.noreply.github.com>
2024-10-20 14:49:54 +0530

    update user.js

--- backend/models/user.js
+++ backend/models/user.js
@@ -29,3 +29,17 @@ const userSchema = new mongoose.Schema({
     email: { type: String, unique: true },
     password: String
 });
+userSchema.pre('save', async function (next) {
+    if (this.isModified('password') || this.isNew) {
+        this.password = await bcrypt.hash(this.password, 10);
+    }
+    next();
+});
+
+userSchema.methods.isValidPassword = async function (password) {
+    return bcrypt.compare(password, this.password);
+};
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
