af39b77197077a16725673aaa554bf980e32ecfb
sahilbd1907 <142887739+sahilbd1907@users.noreply.github.com>
2024-10-20 14:12:55 +0530

    creating video schema

--- backend/models/playlist.js
+++ backend/models/playlist.js
@@ -4,4 +4,10 @@ const fileSchema = new mongoose.Schema({
     filename: String,
     contentType: String,
 });
+const videoSchema = new mongoose.Schema({
+    filename: String,
+    contentType: String,
+    heading: String, // Add a new field for video heading
+    thumbnail: fileSchema,
+});
 
