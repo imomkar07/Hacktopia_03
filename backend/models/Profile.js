2a05b7afef34926dff690d90f6960b583fa0b8e7
sahilbd1907 <142887739+sahilbd1907@users.noreply.github.com>
2024-10-20 14:26:25 +0530

    creating Profile.js

---
+++ backend/models/Profile.js
@@ -0,0 +1,20 @@
+// models/Profile.js
+const mongoose = require('mongoose');
+
+// Define the Profile schema
+const profileSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    profilePic: {
+        type: String
+    }
+});
