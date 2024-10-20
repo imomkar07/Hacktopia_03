c13562f5885158c2f98bab5c1d0475dd4e82ef1a
sahilbd1907 <142887739+sahilbd1907@users.noreply.github.com>
2024-10-20 14:03:24 +0530

    creating message schema

--- backend/models/message.js
+++ backend/models/message.js
@@ -1,1 +1,7 @@
 const mongoose = require('mongoose');
+const messageSchema = new mongoose.Schema({
+    profileId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Profiling',
+        required: true
+    },
